d3b1e1881c56e716c7d7c0b65fa23474cbf58376
Punitesh <ppunitesh@gmail.com>
2022-01-10 20:14:51 +0530

    bootstrap file added and edit

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const dark_theme = `:root{
     --background: #161718;
     --invert_background:#e2e2e2;
 }`
+
 function switch_themes(current) {
     if (current.innerHTML == "Dark") {
         write_to('theme_switcher', 'Light')
@@ -16,11 +17,12 @@ function switch_themes(current) {
     write_to('theme_switcher', 'Dark')
     write_to('style', light_theme)
 }
-function menu_toggel() {
-    document.getElementById('menu').classList.toggle('menu_x')
+function menu_toggel(current) {
+    current.classList.toggle('menu_x')
     document.getElementById('index_menu').classList.toggle('index_menu_x')
 }
 function change_iframe(link) {
     document.getElementById('link_op').src = `./modules/${link}`;
+    
     menu_toggel()
 }
