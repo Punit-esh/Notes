e2efb3e5c9f46d1ba9947dc24a0fb2044f517714
Punitesh <ppunitesh@gmail.com>
2022-01-10 23:22:00 +0530

    jan 10 2022 final edit

---
+++ modules/index_menu.js
@@ -0,0 +1,9 @@
+const HTML = ()=>{
+    return ["Intorduction","Editos","Elements","Attributes","Comments","Styles","Colors","Responsive","Centered","Basic Example","Heading","Paragraphs","Links","Line Break","Horizontal Rule"]
+}
+const CSS = ()=>{
+    return ["Introduction","Syntax and Selectors","Inserting","Colors","Backgrounds","Borders","Margins","Padding","Width and Height","Box Models","Text","Fonts","Icons","Links","Lists","Tables"]
+}
+const JS = ()=>{
+    return ["Overwiew","introduction","Writind Code","Displaying Output","Statements","Syntax","Comments","Variables","Data Types","Operators","Arithematic Operators","Assignment Operators","Comparison operators","Logic & Conditional Operators"]
+}
--- script.js
+++ script.js
@@ -1,28 +1,45 @@
 const write_to = (id, data) => document.getElementById(id).innerHTML = data
 const light_theme = `:root{
     --background: #e2e2e2;
-    --invert_background:#161718;
+    --background-rgb: 226, 226, 226;
+    --invert_background: #161718;
+    --invert_background-rgb: 22, 23, 24;
+
+    --menu_border: #40e0d0;
 }`
 const dark_theme = `:root{
     --background: #161718;
+    --background-rgb: 22, 23, 24;
     --invert_background:#e2e2e2;
+    --invert_background-rgb:226, 226, 226;
+
+    --menu_border: #40e0d0;
 }`
 
+write_to('index_menu',document.getElementsByClassName('main_navbar')[0].innerHTML)
+
+let tab = [...document.getElementsByClassName('main_tabs')];
+tab.forEach(el =>el.addEventListener('click',()=>{
+    tab.forEach(el1=>{
+        el1.classList.remove("active")
+    })
+    el.classList.add('active')
+    make_index(el.dataset.tab)
+}))
+function make_index(id) {
+    let str = eval(`${id}()`).map(el=>`<div class="index_menu_tabs">${el}</div>`).join("");
+    console.log(str);
+    write_to('index_menu',str)
+}
 function switch_themes(current) {
-    if (current.innerHTML == "Dark") {
-        write_to('theme_switcher', 'Light')
-        write_to('style', dark_theme)
-        return
-    }
-    write_to('theme_switcher', 'Dark')
-    write_to('style', light_theme)
+    write_to('theme_switcher', (current.innerHTML=="Light")?'Dark':'Light')
+    write_to('style',(current.innerHTML=="Light")?dark_theme:light_theme)
 }
 function menu_toggel(current) {
     current.classList.toggle('menu_x')
-    document.getElementById('index_menu').classList.toggle('index_menu_x')
+    document.getElementsByClassName('index_menu')[0].classList.toggle('index_menu_x')
 }
 function change_iframe(link) {
-    document.getElementById('link_op').src = `./modules/${link}`;
-    
-    menu_toggel()
+    document.getElementById('link_op').src = `./modules/${link}`;    
+    // menu_toggel()
 }
