0a88e669b97a877142f2514516bf7640b674ecf4
Punitesh <ppunitesh@gmail.com>
2022-01-11 21:36:45 +0530

    jan 11 edit

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 const write_to = (id, data) => document.getElementById(id).innerHTML = data
+const read_from = (id,method) => eval(`document.getElementById(${id}).${method}`)
 const light_theme = `:root{
-    --background: #e2e2e2;
-    --background-rgb: 226, 226, 226;
+    --background: #ffffff;
     --invert_background: #161718;
-    --invert_background-rgb: 22, 23, 24;
 
     --menu_border: #40e0d0;
 }`
 const dark_theme = `:root{
     --background: #161718;
-    --background-rgb: 22, 23, 24;
-    --invert_background:#e2e2e2;
-    --invert_background-rgb:226, 226, 226;
+    --invert_background:#ffffff;
 
     --menu_border: #40e0d0;
 }`
 
 write_to('index_menu',document.getElementsByClassName('main_navbar')[0].innerHTML)
+
+// setInterval(()=>document.getElementById('index_menu').style = `height:${Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)-100}px`,1000)
+document.getElementById('index_menu').style = `height:${Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)-100}px`
 
 let tab = [...document.getElementsByClassName('main_tabs')];
 tab.forEach(el =>el.addEventListener('click',()=>{
@@ -28,7 +28,7 @@ tab.forEach(el =>el.addEventListener('click',()=>{
 }))
 function make_index(id) {
     let str = eval(`${id}()`).map(el=>`<div class="index_menu_tabs">${el}</div>`).join("");
-    console.log(str);
+    // console.log(str);
     write_to('index_menu',str)
 }
 function switch_themes(current) {
@@ -39,7 +39,9 @@ function menu_toggel(current) {
     current.classList.toggle('menu_x')
     document.getElementsByClassName('index_menu')[0].classList.toggle('index_menu_x')
 }
-function change_iframe(link) {
-    document.getElementById('link_op').src = `./modules/${link}`;    
-    // menu_toggel()
-}
+// console.log(read_from("main_navbar","innerHTML"))
+// console.log(document.getElementById("main_navbar").innerHTML)
+// function change_iframe(link) {
+//     document.getElementById('link_op').src = `./modules/${link}`;
+//     // menu_toggel()
+// }
